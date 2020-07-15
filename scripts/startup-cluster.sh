9def79c1726c92827d49cadb13898139530c5df4
Théo Larue <thlarue@live.fr>
2020-07-15 22:33:18 +0200

    minor fixes

--- scripts/startup-cluster.sh
+++ scripts/startup-cluster.sh
@@ -3,6 +3,7 @@
 # Amount of processing units and memory in the system
 CPU_HOST="$(nproc)"
 MEM_HOST="$(cat /proc/meminfo | head -n 1 | awk '{ print $2 }')"
+K3S_TOKEN=${RANDOM}${RANDOM}${RANDOM}
 
 help () {
   cat << EOF
@@ -87,10 +88,13 @@ services:
 
   server:
     image: \"rancher/k3s:\${K3S_VERSION:-latest}\"
-    command: server --no-deploy traefik
+    command: server --disable-agent
+    tmpfs:
+    - /run
+    - /var/run
     privileged: true
     environment:
-    - K3S_CLUSTER_SECRET=somethingtotallyrandom
+    - K3S_TOKEN=$K3S_TOKEN
     - K3S_KUBECONFIG_OUTPUT=/output/kubeconfig.yaml
     - K3S_KUBECONFIG_MODE=666
     volumes:
@@ -98,22 +102,23 @@ services:
     # This is just so that we get the kubeconfig file out
     - .:/output
     ports:
-    - 6443:6443" >> ./docker-compose.yaml
+    - 6443:6443">> ./docker-compose.yaml
 
-i=1
+i=0
 while [ $i -lt $NODES ]
 do
+
   echo "
-  node$i:
-    command: agent --kubelet-arg \"system-reserved=cpu=$(($CPU_HOST - $CPU)),memory=$(($MEM_HOST - $MEM))Ki\"
+  agent$i:
     image: \"rancher/k3s:\${K3S_VERSION:-latest}\"
+    command: agent --kubelet-arg \"system-reserved=cpu=$(($CPU_HOST - $CPU)),memory=$(($MEM_HOST - $MEM))Ki\"
     tmpfs:
     - /run
     - /var/run
     privileged: true
     environment:
     - K3S_URL=https://server:6443
-    - K3S_CLUSTER_SECRET=somethingtotallyrandom" >> ./docker-compose.yaml
+    - K3S_TOKEN=$K3S_TOKEN">> ./docker-compose.yaml
     i=$(($i + 1 ))
 done
 
@@ -122,6 +127,7 @@ volumes:
   k3s-server: {}" >> ./docker-compose.yaml
 
 docker-compose up -d && echo "
-Kubernetes cluster up and running. Try it with kubectl KUBECONFIG=kubeconfig.yaml kubectl get nodes
+Kubernetes cluster up and running. Try it with kubectl: \"KUBECONFIG=kubeconfig.yaml kubectl get nodes\".
+Note that it may take a few minutes to initialize.
 
 To shut down the cluster and delete the images: docker-compose down --rmi all"
