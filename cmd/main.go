7bdadea81a74a0de627b171d8692f82949659e01
Théo Larue <thlarue@live.fr>
2020-07-20 18:22:22 +0200

    read and parse workloads

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube/pkg/translate"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	wlJson := flag.String("w", "", "File specifying a Batsim workload in json format")
+
+	flag.Parse()
+	if flag.NFlag() == 0 {
+		fmt.Fprintf(os.Stderr, "usage:\n\tbatkube-test -w <workload.json> -config <kubeconfig.yaml>\n")
+		os.Exit(1)
+	}
+
+	wl := parseFile(*wlJson)
+
+	fmt.Println(wl)
+}
+
+/*
+Parses a workload file into a byte array
+*/
+func parseFile(file string) translate.Workload {
+	wlFile, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	decoder := json.NewDecoder(wlFile)
+	wlStruct := translate.Workload{}
+	err = decoder.Decode(&wlStruct)
+	if err != nil {
+		panic(err)
+	}
+	return wlStruct
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/oar-team/batkube-test
 
 go 1.14
+
+require gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube v0.0.0-20200717153841-72bdf6bac590
+
+replace gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube => gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube.git v0.0.0-20200717153841-72bdf6bac590
