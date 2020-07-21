8bee349bededd4e1467132a2526943aa3e751153
Théo Larue <thlarue@live.fr>
2020-07-21 16:59:44 +0200

    translation from workload to pods

--- cmd/main.go
+++ cmd/main.go
@@ -4,24 +4,38 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/mitchellh/mapstructure"
 	"gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube/pkg/translate"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-
 	wlJson := flag.String("w", "", "File specifying a Batsim workload in json format")
+	kubeconfig := flag.String("kubeconfig", "", "Path to kubeconfig.yaml")
 
 	flag.Parse()
-	if flag.NFlag() == 0 {
+	if *wlJson == "" || *kubeconfig == "" {
 		fmt.Fprintf(os.Stderr, "usage:\n\tbatkube-test -w <workload.json> -config <kubeconfig.yaml>\n")
 		os.Exit(1)
 	}
 
 	wl := parseFile(*wlJson)
 
-	fmt.Println(wl)
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	submitJobs(cs, translateJobsToPods(&wl))
 }
 
 /*
@@ -30,13 +44,73 @@ Parses a workload file into a byte array
 func parseFile(file string) translate.Workload {
 	wlFile, err := os.Open(file)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	decoder := json.NewDecoder(wlFile)
-	wlStruct := translate.Workload{}
-	err = decoder.Decode(&wlStruct)
+	// First step : decode into a map
+	jsonData := make(map[string]interface{}, 0)
+	err = decoder.Decode(&jsonData)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-	return wlStruct
+
+	// Second step : decode again into a workload and decode profiles.
+	wl := translate.Workload{}
+	if err := mapstructure.Decode(jsonData, &wl); err != nil {
+		log.Fatal(err)
+	}
+	for profileName, profileData := range jsonData["profiles"].(map[string]interface{}) {
+		profile := wl.Profiles[profileName]
+		profileDataMap := profileData.(map[string]interface{})
+
+		// Profile specs
+		if err := mapstructure.Decode(profileDataMap, &profile.Specs); err != nil {
+			log.Fatal(err)
+		}
+		delete(profile.Specs, "type")
+		delete(profile.Specs, "ret")
+		wl.Profiles[profileName] = profile
+	}
+	return wl
+}
+
+func translateJobsToPods(wl *translate.Workload) []*v1.Pod {
+	simData := translate.SimulationBeginsData{
+		Profiles: make(map[string]map[string]translate.Profile, 0),
+	}
+	// Only one workload for now
+	simData.Profiles["w0"] = make(map[string]translate.Profile, 0)
+	for profileName, profile := range wl.Profiles {
+		simData.Profiles["w0"][profileName] = profile
+	}
+
+	pods := make([]*v1.Pod, 0)
+	for _, job := range wl.Jobs {
+		// JobToPod takes as input a JOB_SUBMITTED event, where the id is workload!jobId.
+		job.Id = "w0!" + job.Id
+		err, pod := translate.JobToPod(job, simData)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// models.IoK8sAPICoreV1Pod -> v1.Pod
+		// Translation is done thanks to the json tags that remain the same.
+		corev1Pod := v1.Pod{}
+		podBytes, err := json.Marshal(pod)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err = json.Unmarshal(podBytes, &corev1Pod); err != nil {
+			log.Fatal(err)
+		}
+		pods = append(pods, &corev1Pod)
+	}
+	return pods
+}
+
+/*
+Submits the given jobs at the correct timestamps.
+*/
+func submitJobs(cs *kubernetes.Clientset, pods []*v1.Pod) {
+	//TODO
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,21 @@ module github.com/oar-team/batkube-test
 
 go 1.14
 
-require gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube v0.0.0-20200717153841-72bdf6bac590
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/imdario/mergo v0.3.10 // indirect
+	github.com/mitchellh/mapstructure v1.3.2
+	gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube v0.0.0-00010101000000-000000000000
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/api v0.18.6
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20200720150651-0bdb4ca86cbc // indirect
+)
 
-replace gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube => gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube.git v0.0.0-20200717153841-72bdf6bac590
+replace (
+	gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube => gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube.git v0.0.0-20200721143233-acc0de1ca11e
+	k8s.io/api => k8s.io/api v0.18.0
+	k8s.io/apimachinery => k8s.io/apimachinery v0.18.2-beta.0
+	k8s.io/client-go => k8s.io/client-go v0.18.0
+)
