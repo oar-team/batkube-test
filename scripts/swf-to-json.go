f2a41e176fa072e8c0a2609514b67450fc3feea8
Théo Larue <thlarue@live.fr>
2020-07-24 18:19:05 +0200

    translation done

--- scripts/swf-to-json.go
+++ scripts/swf-to-json.go
@@ -2,36 +2,91 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
+	"strconv"
 
-	log "github.com/sirupsen/logrus"
+	"gitlab.com/ryax-tech/internships/2020/scheduling_simulation/batkube/pkg/translate"
 )
 
 func main() {
 	filePath := flag.String("in", "", "input csv file with swf format")
+	outPath := flag.String("out", "", "output file")
 	flag.Parse()
-	if *filePath == "" {
+	if *filePath == "" || *outPath == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 	f, err := os.Open(*filePath)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+
+	out, err := os.Create(*outPath)
+	if err != nil {
+		panic(err)
 	}
 
 	r := csv.NewReader(f)
-	r.Comma = ' '
 	r.Comment = ';'
+	r.Comma = ' '
 
-	line, err := r.Read()
-	if err != nil {
-		log.Fatal(err)
+	wl := translate.Workload{
+		NbRes:    1,
+		Jobs:     make([]translate.Job, 0),
+		Profiles: make(map[string]translate.Profile),
 	}
-	line = formatLine(line)
+	line, _ := r.Read()
+	for line != nil {
+		// There are many blank spaces
+		line = parseLine(line)
+
+		// Extract the necessary info
+		subtime, err := strconv.ParseFloat(line[1], 64)
+		if err != nil {
+			panic(err)
+		}
+		runTime, err := strconv.ParseFloat(line[3], 64)
+		if err != nil {
+			panic(err)
+		}
+		cpu := line[4]
+
+		// Create the profile if it does not exist
+		profileName := fmt.Sprintf("delay%f", runTime)
+		_, ok := wl.Profiles[profileName]
+		if !ok {
+			wl.Profiles[profileName] = translate.Profile{
+				Type: "delay",
+				Ret:  1,
+				Specs: map[string]interface{}{
+					"scheduler": "default",
+					"delay":     runTime,
+					"cpu":       cpu,
+				},
+			}
+		}
+
+		job := translate.Job{
+			Id:      line[0],
+			Subtime: subtime,
+			Res:     1,
+			Profile: profileName,
+		}
+		wl.Jobs = append(wl.Jobs, job)
+
+		line, _ = r.Read()
+	}
+
+	encoder := json.NewEncoder(out)
+	if err = encoder.Encode(wl); err != nil {
+		panic(err)
+	}
 }
 
-func formatLine(line []string) []string {
+func parseLine(line []string) []string {
 	formattedLine := make([]string, 0)
 	for _, col := range line {
 		if col != "" {
