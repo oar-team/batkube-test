9ca1c0ed2e04bb77f526c21646a3c0746367fa94
Théo Larue <thlarue@live.fr>
2020-08-06 00:29:45 +0200

    fixes and improvements

--- scripts/min-delay-expe.sh
+++ scripts/min-delay-expe.sh
@@ -8,9 +8,9 @@ BATKUBE=../batkube/batkube
 
 # min delay starting and ending values in ms
 START=0
-END=100
-N=50 # number of points to compute
-PASSES=10 # number of trials per point
+END=50
+STEP=1
+PASSES=20 # number of trials per point
 
 OUT="expe-out/min-delay-$(basename $W | cut -f 1 -d '.').csv"
 
@@ -22,41 +22,47 @@ if [ -f "$OUT" ]; then
     echo "exiting"
     exit
   fi
-  echo
 fi
 
 
 touch "$OUT"
-echo "delay success_rate mean_failure_sim_time mean_success_sim_time" > $OUT
+echo "delay success_rate mean_failure_sim_time min_failure_sim_time max_failure_sim_time mean_success_sim_time min_success_sim_time max_success_sim_time" > $OUT
 
 killall batsim > /dev/null 2>&1
 killall scheduler > /dev/null 2>&1
 killall batkube > /dev/null 2>&1
 
-step=$(( ($END-$START)/$N ))
+n=$(echo "scale=0; ($END - $START) / $STEP" | bc)
 
 delay=$START
 j=0
 exp_start=$(date +%s.%N)
 while [ $delay -lt $END ]; do
 
-  echo "=======min-delay = $delay (from $START to $END, step $j out of $N)======="
-  echo
+  echo -e "\n=======min-delay=${delay}ms (step $j/$n)======="
 
   successes=0
   total_success_sim_time=0
+  max_success_sim_time=0
+  min_success_sim_time=0
   total_failure_sim_time=0
+  max_failure_sim_time=0
+  min_failure_sim_time=0
+
+  echo "" > batsim.log
+  echo "" > scheduler.log
+  echo "" > batkube.log
 
   i=0
   step_start=$(date +%s.%N)
   while [ $i -lt $PASSES ]; do
-    echo "" > batsim.log
-    echo "" > scheduler.log
-    echo "" > batkube.log
+    echo -n "Pass $(( $i + 1 )) out of $PASSES..."
 
-    echo "Pass $(( $i + 1 )) out of $PASSES"
-
-    echo "Launching Batkube and the scheduler"
+    while [ "$(lsof -i -P -n | grep :27000 | wc -l)" -eq 1 ]; do
+      echo e "\nWarning: port 27000 already in use. Trying to shut down the scheduler..."
+      pkill scheduler
+      sleep 2
+    done
     $SCHED --kubeconfig="$KUBECONFIG" --kube-api-content-type=application/json --leader-elect=false --scheduler-name=default > scheduler.log 2>&1 &
     sched_pid=$!
 
@@ -64,7 +70,6 @@ while [ $delay -lt $END ]; do
     batkube_pid=$!
     sleep 4 # give time for the api to start
 
-    echo "Simulation starts"
     start=$(date +%s.%N)
     batsim -p "$P" -w "$W" -e "expe-out/min-delay" --enable-compute-sharing > batsim.log 2>&1 &
     batsim_pid=$!
@@ -73,30 +78,49 @@ while [ $delay -lt $END ]; do
     exit_code=$?
     duration=$(echo "$(date +%s.%N) - $start" | bc)
 
-    echo "Result: exit code $exit_code; simulation time $duration"
-
     kill $sched_pid > /dev/null 2>&1
     kill $batkube_pid > /dev/null 2>&1
     kill $batsim_pid > /dev/null 2>&1
+    sleep 1 # wait for zmq to close properly
 
-    if [ $exit_code -gt 1 ]; then
-      echo "Unexpected exit code from Batkube: $exit_code. Retrying."
-      echo
+    [ $exit_code -gt 1 ] && \
+      echo "Unexpected exit code from Batkube: $exit_code. Retrying." && \
       continue
-    fi
-    ((successes+= 1 - exit_code))
+
     if [ $exit_code -eq 1 ]; then
+      echo "failed (${duration}s)"
       total_failure_sim_time=$(echo "scale=3; $total_failure_sim_time + $duration" | bc)
+
+      [ "$(echo "$duration > $max_failure_sim_time" | bc)" -eq 1 ] && \
+        max_failure_sim_time=$duration
+
+      [ "$(echo "$duration < $min_failure_sim_time" | bc)" -eq 1 -o \
+        "$(echo "$min_failure_sim_time == 0" | bc)" -eq 1 ] && \
+        min_failure_sim_time=$duration
+
     else
+      echo "passed (${duration}s)"
+      ((successes++))
       total_success_sim_time=$(echo "scale=3; $total_success_sim_time + $duration" | bc)
+
+      [ "$(echo "$duration > $max_success_sim_time" | bc)" -eq 1 ] && \
+        max_success_sim_time=$duration
+
+      [ "$(echo "$duration < $min_success_sim_time" | bc)" -eq 1 -o \
+        "$(echo "$min_success_sim_time == 0" | bc)" -eq 1 ] && \
+        min_success_sim_time=$duration
     fi
 
     ((i++))
-    echo
   done
 
   step_duration=$(echo "$(date +%s.%N) - $step_start" | bc)
-  success_rate=$(echo "scale=2; $successes / $PASSES" | bc)
+  i=2
+  success_rate=0
+  while [ $successes -gt 0 -a $(echo "$success_rate == 0" | bc) -eq 1 ]; do
+    success_rate=$(echo "scale=${i}; $successes / $PASSES" | bc)
+    ((i++))
+  done
   if [ $successes -gt 0 ]; then
     mean_success_sim_time=$(echo "scale=3; $total_success_sim_time / $successes" | bc)
   else
@@ -104,16 +128,18 @@ while [ $delay -lt $END ]; do
   fi
   mean_failure_sim_time=$(echo "scale=3; $total_failure_sim_time / ($PASSES-$successes)" | bc)
   ((j++))
-  echo "$delay $success_rate $mean_failure_sim_time $mean_success_sim_time" >> $OUT
+  echo "$delay $success_rate $mean_failure_sim_time $min_failure_sim_time $max_failure_sim_time $mean_success_sim_time $min_success_sim_time $max_success_sim_time" >> $OUT
 
-  echo "Step done in $step_duration s"
+  echo -e "\nStep done in ${step_duration}s"
   echo "Success rate $success_rate"
   echo "Avg success sim time $mean_success_sim_time"
   echo "Avg failure sim time $mean_failure_sim_time"
 
-  ((delay+=$step))
-  echo
+  rough_eta=$(echo "$step_duration * ($n - $j)" | bc)
+  echo -e "\nETA: $(date -d@$rough_eta -u +%Hh%Mh%Ss)"
+
+  ((delay+=$STEP))
 done
 
 exp_duration=$(echo "$(date +%s.%N) - $exp_start" | bc)
-echo "Experience lasted $exp_duration s"
+echo "Experience lasted ${exp_duration}s"
